e6d9d899c5a11dad94d279e6169c9c7fd200b26f
Radzi0slaw-102 <radek.klamka@gmail.com>
2026-07-15 10:30:29 +0200

    ✨ Add GraphRAGExtractir class and extraction prompt

--- graphRAG/mlm_tutorial/src/config.py
+++ graphRAG/mlm_tutorial/src/config.py
@@ -4,3 +4,31 @@ CHUNK_OVERLAP = 20
 MAX_PATHS_PER_CHUNK = 2
 MAX_CLUSTER_SIZE = 5
 DATA_URL = "https://raw.githubusercontent.com/tomasonjo/blog-datasets/main/news_articles.csv"
+KG_TRIPLET_EXTRACT_TMPL = """
+-Goal-
+Given a text document, identify all entities and their entity types from the text and all relationships among the identified entities.
+Given the text, extract up to {max_knowledge_triplets} entity-relation triplets.
+
+-Steps-
+1. Identify all entities. For each identified entity, extract the following information:
+- entity_name: Name of the entity, capitalized
+- entity_type: Type of the entity
+- entity_description: Comprehensive description of the entity's attributes and activities
+Format each entity as ("entity")
+
+2. From the entities identified in step 1, identify all pairs of (source_entity, target_entity) that are *clearly related* to each other.
+For each pair of related entities, extract the following information:
+- source_entity: name of the source entity, as identified in step 1
+- target_entity: name of the target entity, as identified in step 1
+- relation: relationship between source_entity and target_entity
+- relationship_description: explanation as to why you think the source entity and the target entity are related to each other
+
+Format each relationship as ("relationship")
+
+3. When finished, output.
+
+-Real Data-
+######################
+text: {text}
+######################
+output:"""
